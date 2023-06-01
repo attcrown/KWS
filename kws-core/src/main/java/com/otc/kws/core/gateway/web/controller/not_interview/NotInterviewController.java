45be6e429b0a6de8fe6a1be3be865b35399d5f8e
attcrwon <ittatting@gmail.com>
2023-06-01 16:26:51 +0700

    add_page_ques_notinter

---
+++ kws-core/src/main/java/com/otc/kws/core/gateway/web/controller/not_interview/NotInterviewController.java
@@ -0,0 +1,22 @@
+package com.otc.kws.core.gateway.web.controller.not_interview;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.otc.kws.core.gateway.web.controller.workspace.KwsCoreWorkspaceController;
+@Controller
+@RequestMapping(KwsCoreWorkspaceController.PREFIX_PATH + "/hr/not_interview")
+
+public class NotInterviewController extends KwsCoreWorkspaceController{
+
+	@Override
+	protected void setupModel(HttpServletRequest request, HttpServletResponse response, Model model) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
