5a1f43437bae4f69104fe3e97b7d1c77f45fc51a
arrrin <69157252+arrrin@users.noreply.github.com>
2023-10-31 14:32:00 +0700

    Develop: Update สะสมชั่วโมง

---
+++ kws-core/src/main/java/com/otc/kws/core/gateway/web/controller/hour_collect_report/report.java
@@ -0,0 +1,20 @@
+package com.otc.kws.core.gateway.web.controller.hour_collect_report;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.otc.kws.core.gateway.web.controller.workspace.KwsCoreWorkspaceController;
+@Controller
+@RequestMapping(KwsCoreWorkspaceController.PREFIX_PATH + "/ocp_user/hour_collect/report")
+public class report extends KwsCoreWorkspaceController{
+
+	@Override
+	protected void setupModel(HttpServletRequest request, HttpServletResponse response, Model model) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
